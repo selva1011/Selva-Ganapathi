44279177f053c552c42d5acd95b0feff02d564b1
Selva Ganapathi <60595640+selva1011@users.noreply.github.com>
2023-12-15 12:53:44 +0530

    vercel added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Hero, About, Projects, Contact } from "./pages";
 import { NavBar } from "./components";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export default function App() {
   return (
     <main className="relative debug-screens bg-apple-white">
+      <SpeedInsights />
       <NavBar />
       <section>
         <Hero />
