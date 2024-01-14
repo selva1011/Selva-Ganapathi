687a8803da66c0a9cbbfebee5127f200ff8a08e3
Selva Ganapathi <60595640+selva1011@users.noreply.github.com>
2024-01-14 19:41:32 +0530

    avif converted

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,6 @@
 import footer_sign from "./footer_sign.png";
 import profilepic4 from "./profile-pic_c.webp";
 import hero from "./site1.png";
-import anim from "./animated.avif";
+import anim from "./animated.webp";
 
 export { footer_sign, profilepic4, hero, anim };
